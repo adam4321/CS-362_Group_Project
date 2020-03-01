61b7de28a8e96fea09a671f2fc0d0f77ad005ce1
elliochr <46430710+elliochr@users.noreply.github.com>
2020-02-29 16:45:34 -0800

    fixed bad typo

--- Group7/UnitTestOnly/test_calculator.py
+++ Group7/UnitTestOnly/test_calculator.py
@@ -20,9 +20,9 @@ class Test_Operations(TestCase):
         test_val2 = calculator.multiplication(-5, 6)
         test_val3 = calculator.multiplication(5.3, 6.7)
 
-        self.assertTrue(test_val1, 30)
-        self.assertTrue(test_val2, -30)
-        self.assertTrue(test_val3, 35.51)
+        self.assertEqual(test_val1, 30)
+        self.assertEqual(test_val2, -30)
+        self.assertEqual(test_val3, 35.51)
 
     def test_division(self):
         test_val1 = calculator.division(4, 2)
@@ -30,10 +30,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.division(5.8, 2.5)
         test_val4 = calculator.division(5.3, 6.7)
 
-        self.assertTrue(test_val1, 2)
-        self.assertTrue(test_val2, -2.5)
-        self.assertTrue(test_val3, 2.32)
-        self.assertTrue(test_val4, 0.791045)
+        self.assertEqual(test_val1, 2)
+        self.assertEqual(test_val2, -2.5)
+        self.assertEqual(test_val3, 2.32)
+        self.assertEqual(test_val4, 0.791045)
 
     def test_square_root(self):
         test_val1 = calculator.square_root(16)
@@ -41,10 +41,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.square_root(64)
         test_val4 = calculator.square_root(73.96)
 
-        self.assertTrue(test_val1, 4)
-        self.assertTrue(test_val2, 2)
-        self.assertTrue(test_val3, 8)
-        self.assertTrue(test_val4, 8.6)
+        self.assertEqual(test_val1, 4)
+        self.assertEqual(test_val2, 2)
+        self.assertEqual(test_val3, 8)
+        self.assertEqual(test_val4, 8.6)
         self.assertAlmostEqual(2,  calculator.square_root(5), 2)
 
     def test_power_two(self):
@@ -53,10 +53,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.power_two(8)
         test_val4 = calculator.power_two(8.6)
 
-        self.assertTrue(test_val1, 16)
-        self.assertTrue(test_val2, 4)
-        self.assertTrue(test_val3, 64)
-        self.assertTrue(test_val4, 73.96)
+        self.assertEqual(test_val1, 16)
+        self.assertEqual(test_val2, 4)
+        self.assertEqual(test_val3, 64)
+        self.assertEqual(test_val4, 73.96)
 
     def test_inverse(self):
         test_val1 = calculator.inverse(4)
@@ -64,10 +64,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.inverse(8)
         test_val4 = calculator.inverse(8.6)
 
-        self.assertTrue(test_val1, 0.25)
-        self.assertTrue(test_val2, -0.5)
-        self.assertTrue(test_val3, 0.125)
-        self.assertTrue(test_val4, 0.116279)
+        self.assertEqual(test_val1, 0.25)
+        self.assertEqual(test_val2, -0.5)
+        self.assertEqual(test_val3, 0.125)
+        self.assertEqual(test_val4, 0.116279)
 
     def test_factorial(self):
         test_val1 = calculator.factorial(-1)
@@ -75,10 +75,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.factorial(1)
         test_val4 = calculator.factorial(5)
 
-        self.assertTrue(test_val1, "Invalid Input")
-        self.assertTrue(test_val2, 1)
-        self.assertTrue(test_val3, 1)
-        self.assertTrue(test_val4, 120)
+        self.assertEqual(test_val1, "Invalid Input")
+        self.assertEqual(test_val2, 1)
+        self.assertEqual(test_val3, 1)
+        self.assertEqual(test_val4, 120)
         self.assertAlmostEqual(1,  calculator.factorial(1.2), 2)
 
     def test_absolute_val(self):
@@ -87,10 +87,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.absolute_val(1)
         test_val4 = calculator.absolute_val(-5.5)
 
-        self.assertTrue(test_val1, 1)
-        self.assertTrue(test_val2, 0)
-        self.assertTrue(test_val3, 1)
-        self.assertTrue(test_val4, 5.5)
+        self.assertEqual(test_val1, 1)
+        self.assertEqual(test_val2, 0)
+        self.assertEqual(test_val3, 1)
+        self.assertEqual(test_val4, 5.5)
 
     def test_sine(self):
         test_val1 = calculator.sine(1)
@@ -98,10 +98,10 @@ class Test_Operations(TestCase):
         test_val3 = calculator.sine(1.5)
         test_val4 = calculator.sine(2.58)
 
-        self.assertTrue(test_val1, 0.841471)
-        self.assertTrue(test_val2, 0)
-        self.assertTrue(test_val3, 0.997495)
-        self.assertTrue(test_val4, 0.532535)
+        self.assertEqual(test_val1, 0.841471)
+        self.assertEqual(test_val2, 0)
+        self.assertEqual(test_val3, 0.997495)
+        self.assertEqual(test_val4, 0.532535)
 
     def test_cosine(self):
         test_val1 = calculator.cosine(1)
@@ -110,6 +110,6 @@ class Test_Operations(TestCase):
         test_val4 = calculator.cosine(2.58)
 
         self.assertAlmostEqual(test_val1, 540302, 5)
-        self.assertTrue(test_val2, 1)
-        self.assertTrue(test_val3, 0.070737)
-        self.assertTrue(test_val4, -0.846408)
+        self.assertEqual(test_val2, 1)
+        self.assertEqual(test_val3, 0.070737)
+        self.assertEqual(test_val4, -0.846408)
